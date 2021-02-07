d5e470c82d5bacecb4ccfeec5fc1ec8f35cbb90e
talha915 <talhazafar915426@gmail.com>
2021-02-07 06:33:24 -0800

    Working Model Post

--- Nodejs/models/post.js
+++ Nodejs/models/post.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 
 const post = mongoose.Schema.Types({
-    
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imgUrl: {
+        type: String,
+        required: true
+    }
 });
